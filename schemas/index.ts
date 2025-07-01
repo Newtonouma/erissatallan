793afa10dc0dae7797d3ab94a2eaa1dc8ee0465b
Newtonouma <armstrongnewton02@gmail.com>
2025-07-01 12:13:46 +0300

    added well

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,8 +4,8 @@ import project from "./project";
 import post from "./post";
 import author from "./author";
 import heroe from "./heroe";
-import { youtube } from "./youtube";
-import { table } from "./table";
+import youtube from "./youtube";
+import table from "./table";
 import blockContent from "./blockContent";
 import quiz from "./quiz";
 
