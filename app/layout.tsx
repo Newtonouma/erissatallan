4fb417f254fe4a827eab2d24a4b6c8b1858d0e19
aerissat <aerissat@gmail.com>
2025-07-02 18:03:39 +0300

    Changes to main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,22 +15,22 @@ const inter = Inter({
 });
 
 const options = {
-  title: "Victor Eke | Software Developer",
+  title: "Allan Erissat | AI Engineer",
   description:
     "Victor Eke is a Software Developer and Technical Writer who is passionate about building solutions and contributing to open source communities",
-  url: "https://victoreke.com",
+  url: "victoreke.com",
   ogImage:
     "https://res.cloudinary.com/victoreke/image/upload/v1692635746/victoreke/og.png",
 };
 
 export const metadata: Metadata = {
   title: options.title,
-  metadataBase: new URL(options.url),
+  //metadataBase: new URL(options.url),
   description: options.description,
   openGraph: {
     title: options.title,
     url: options.url,
-    siteName: "victoreke.com",
+    siteName: "aerissat.com",
     locale: "en-US",
     type: "website",
     description: options.description,
